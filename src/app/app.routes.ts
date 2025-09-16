26a9c782a8c367e01d59fa41913f5104896f58f3
Victoria AF <fafascinada@gmail.com>
2025-09-16 12:15:18 +0200

    creadas pages (home, user-detail, user-form) y componente user-card con sistema de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { UserDetail } from './pages/user-detail/user-detail';
+import { UserForm } from './pages/user-form/user-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: Home },
+    { path: 'user/:id', component: UserDetail},
+    { path: 'newuser', component: UserForm },
+    { path: 'updateuser/:_id', component: UserForm }
+];
